5b55fe560b0de5a870cd8c184b80e9ddbd652d5d
FirmWareBuilder <fw@static.172-31-1-100.clients.your-server.de>
2016-05-26 22:04:31 +0200

    in progress

---
+++ site-builder.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+# Script der Community Freifunk-Donau-Ries.de
+# zur Erstellung der Firmware-Images
+# für mehrere Hardware-Architekturen
+# für mehrere Domänen
+# 
+###############################################################################################
+# Buildscript zur Erstellung der Images
+# 
+# Dieses Script holt die passende Gluon-Version von GitHub und überträgt die Gluon-Konfiguration
+#
+###############################################################################################
+ 
+
